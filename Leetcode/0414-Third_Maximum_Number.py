0412b919e3dcd69fc6ccf6c7e388356bbb6b28e8
Jiaming Ke <JiamingKe@outlook.com>
2019-04-16 16:17:54 +0100

    Update 0414-Third_Maximum_Number.py

--- Leetcode/0414-Third_Maximum_Number.py
+++ Leetcode/0414-Third_Maximum_Number.py
@@ -1,10 +1,7 @@
 import heapq
-class Solution(object):
-    def thirdMax(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
         nums = list(set(nums))
         if len(nums)<3:
             return max(nums)
